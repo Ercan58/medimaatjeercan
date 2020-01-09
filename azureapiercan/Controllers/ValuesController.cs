2e665b93c23c678232c515842f98b0e39becafe8
Ercan58 <33756300+Ercan58@users.noreply.github.com>
2020-01-09 15:37:39 +0100

    changes
    changes

--- azureapiercan/Controllers/ValuesController.cs
+++ azureapiercan/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace azureapiercan.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "ercan", "kalan" };
         }
 
         // GET api/values/5
